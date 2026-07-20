612e349871c7f9db875111b57c3d3d922dbbfbff
gonzalo <larroyagonzalo+git@gmail.com>
2026-07-20 20:26:27 +0200

    fix build error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-/** @type {import('next').NextConfig} */
-const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '');
-const basePath = repo ? `/${repo}` : '';
-
 const nextConfig = {
   output: 'export',
-  basePath,
   images: {
     unoptimized: true,
   },
